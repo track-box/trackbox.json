a62aebde62f07aa24eb5e3994622be47f76039f9
tattii <yuta.tatti@gmail.com>
2016-11-14 23:17:38 +0900

    set env production

--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,11 @@ module.exports = {
 	resolve: {
 		extensions: ['', '.js', '.jsx']
 	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		})
+	],
 }
