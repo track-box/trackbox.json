b120913ece911cc3c133d0397e6ac81f5615f47f
tattii <yuta.tatti@gmail.com>
2016-11-15 00:12:34 +0900

    fix

--- src/components/ConsoleFAB.js
+++ src/components/ConsoleFAB.js
@@ -14,10 +14,17 @@ const iconStyle = {
 	color: "#424242"
 };
 
-const ConsoleFAB = (props) => (
-	<FloatingActionButton style={style} mini={true} backgroundColor={blue50} {...props}>
-		<FontIcon className="material-icons" style={iconStyle}>assessment</FontIcon>
-	</FloatingActionButton>
-);
+const ConsoleFAB = (props) => {
+	if (/iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase()) && 
+		/twitter|fbav|line/.test(navigator.userAgent.toLowerCase())){
+		style.bottom = 100
+	}
+
+	return (
+		<FloatingActionButton style={style} mini={true} backgroundColor={blue50} {...props}>
+			<FontIcon className="material-icons" style={iconStyle}>assessment</FontIcon>
+		</FloatingActionButton>
+	)
+}
 
 export default ConsoleFAB;
