96652b0f9aa8497e8beddd6af670721fbf22ec0b
tattii <yuta.tatti@gmail.com>
2016-11-19 05:02:15 +0900

    add maps

--- src/components/SelectMap.js
+++ src/components/SelectMap.js
@@ -27,8 +27,11 @@ export default class SelectMap extends React.Component {
 					style={{ width:"100%" }}
 				>
 					<MenuItem value={"none"} primaryText="" />
-					<MenuItem value={"Saga2016"} primaryText="Saga2016" />
+					<MenuItem value={"Watarase2016"} primaryText="Watarse2016" />
+					<MenuItem value={"Saku2016"} primaryText="Saku2016" />
+					<MenuItem value={"Suzuka2016"} primaryText="Suzuka2016" />
 					<MenuItem value={"Ichinoseki2016"} primaryText="Ichinoseki2016" />
+					<MenuItem value={"Saga2016"} primaryText="Saga2016" />
 				</SelectField>
 			</Dialog>
 		)
